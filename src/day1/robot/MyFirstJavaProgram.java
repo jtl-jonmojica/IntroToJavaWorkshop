befd7582b6ee20ede65a1215657ebe1a641a35f1
jtl-jonmojica <league@Mac-mini-3.local>
2016-12-28 11:49:52 -0800

    i did coding with the best and smartest teacher ever

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,20 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot chewbaca=new Robot();
+chewbaca.setPenColor(255,0,0);
+chewbaca.setSpeed(10);
+chewbaca.penDown();
+chewbaca.move(500);
+chewbaca.turn(90);		
+chewbaca.move(500);
+chewbaca.turn(90);
+chewbaca.move(500);
+chewbaca.turn(90);	
+chewbaca.move(500);	
+for (int i = 0; i < 1010; i++) {
+	chewbaca.turn(10);
+}
+ }
 
-	
-		
-		
-	}
 }
